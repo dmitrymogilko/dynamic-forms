e542e1fbf878c668c67fe3e27ca28bfeed5a473a
Dmitry Mogilko <dimitry323@gmail.com>
2021-06-23 15:30:05 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Form from "@rjsf/core";
+import { Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const log = (type) => console.log.bind(console, type);
+
+
+export default class App extends React.Component {
+    state = {
+        schema : {},
+        formData: {},
+        currentScmType:'jira'
+    }
+
+    componentDidMount() {
+        this.setScmForm(this.state.currentScmType);
+    }
+
+    setScmForm(scmId){
+        // form itself
+        const urlForm = `${scmId}.json`;
+        axios.get(urlForm)
+            .then(res => {
+                const schema = res.data;
+                this.setState({
+                    schema
+                });
+         });
+        //fetch form data
+        const  urlFormData = `${scmId}.data.json`;
+        axios.get(urlFormData)
+            .then(res => {
+                const formData = res.data;
+                this.setState({
+                    formData
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div>
+               <div>
+                   <select onChange={event => this.setScmForm(event.target.value)}>
+                       <option selected value="jira">Jira</option>
+                       <option value="bitbucket">Bitbucket</option>
+                       <option value="azure">Azure</option>
+                   </select>
+               </div>
+                <Form schema={this.state.schema}
+                  formData={this.state.formData}
+                  liveValidate
+                  onChange={log("changed")}
+                  onSubmit={log("submitted")}
+                  onError={log("errors")} />
+            </div>
+
+
+        )
+    }
 }
-
-export default App;
